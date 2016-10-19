aae973e6292a844aa7238e4ef333dfd47d238234
prasmus <rasmus.prees@tptlive.ee>
2016-10-19 14:14:05 +0300

    muudatus

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("How are you?");
+        System.out.print("Goodbye, ");
+        System.out.println("cruel world");
+        System.out.print("Hello!\nHow are you?\n");
+        System.out.println("She said \"Hello!\" to me.");
+
+        kell.kell1();
+    }
+}
+
---
+++ src/kell.java
@@ -0,0 +1,18 @@
+/**
+ * Created by opilane on 19.10.2016.
+ */
+public class kell {
+    public static void kell1 () {
+    int hour = 11;
+    int minute = 59;
+    System.out.print("The current time is ");
+        System.out.print(hour);
+        System.out.print(":");
+        System.out.print(minute);
+        System.out.println(".");
+        System.out.print("Number of minutes since midnight: ");
+        System.out.println(hour * 60 + minute);
+
+
+        }
+}
